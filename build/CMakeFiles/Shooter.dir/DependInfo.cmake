f9b7571b34d618acf142a7bbe129aa67b6abb3b3
Adam Lind <adamlind@live.se>
2018-07-10 12:09:43 +1000

    Initial commit

---
+++ build/CMakeFiles/Shooter.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/adam/Development/Shooter2.0/src/Character.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/Character.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/Collision.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/Collision.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/Combat.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/Combat.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/Game.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/Game.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/GameObject.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/GameObject.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/TextureManager.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/TextureManager.cpp.o"
+  "/Users/adam/Development/Shooter2.0/src/main.cpp" "/Users/adam/Development/Shooter2.0/build/CMakeFiles/Shooter.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Shooter.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shooter.dir/src/main.cpp.o"
+  "CMakeFiles/Shooter.dir/src/Game.cpp.o"
+  "CMakeFiles/Shooter.dir/src/Character.cpp.o"
+  "CMakeFiles/Shooter.dir/src/Collision.cpp.o"
+  "CMakeFiles/Shooter.dir/src/TextureManager.cpp.o"
+  "CMakeFiles/Shooter.dir/src/GameObject.cpp.o"
+  "CMakeFiles/Shooter.dir/src/Combat.cpp.o"
+  "../bin/Shooter.pdb"
+  "../bin/Shooter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
